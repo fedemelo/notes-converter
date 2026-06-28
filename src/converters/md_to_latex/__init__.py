cb814ceef3fa735929bc830c46826276d4f1bed8
Federico Melo <f.melo@uniandes.edu.co>
2026-06-27 23:02:18 -0500

    Delete former inits

--- src/converters/md_to_latex/__init__.py
+++
@@ -1,7 +0,0 @@
-from .parsers.obsidian import ObsidianMarkdownParser
-from .renderers.latex import LatexRenderer
-
-
-def convert_md_to_latex(source: str) -> str:
-    doc = ObsidianMarkdownParser().parse(source)
-    return LatexRenderer().render(doc)
