f9d189021f8df9e170aa53e36a80cef697ac7361
Federico Melo <f.melo@uniandes.edu.co>
2026-06-27 23:05:18 -0500

    Implement md to latex converter

---
+++ src/converters/md_to_latex.py
@@ -0,0 +1,8 @@
+from src.parsers.markdown.obsidian import ObsidianMarkdownParser
+from src.renderers.latex import LatexRenderer
+
+_parser = ObsidianMarkdownParser()
+_renderer = LatexRenderer()
+
+def convert_md_to_latex(text: str) -> str:
+    return _renderer.render(_parser.parse(text))
